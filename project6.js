dfc13323f38c194bb5c671796e850139055e5650
farhanjamil1 <150520713+farhanjamil1@users.noreply.github.com>
2023-11-13 21:19:55 +0500

    Add files via upload
    Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.

---
+++ project6.js
@@ -0,0 +1,4 @@
+var personName = "\t           Farhan Jamil       \n";
+console.log("Name with whitespace:", personName);
+var strippedName = personName.trim();
+console.log("strippedName:", strippedName);
---
+++ project6.ts
@@ -0,0 +1,6 @@
+let personName: string = "\t           Farhan Jamil       \n"
+console.log ("Name with whitespace:" , personName)
+
+let strippedName: string = personName.trim()
+
+console.log ("strippedName:",strippedName)
